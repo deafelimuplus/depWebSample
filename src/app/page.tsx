1f6e3e44292390452be85db25132471f7f5ab14e
deafelimuplus <47976598+hudsonasi@users.noreply.github.com>
2024-10-30 20:53:32 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const message = process.env["MESSAGE"] || "finally it works!!!";
   return (
     <main className="content">
-      <h1 className="heading">Hello Sofia!</h1>
+      <h1 className="heading">Hello Sofia and passed!</h1>
       <p>{message}</p>
     </main>
   );
