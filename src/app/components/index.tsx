58361c5514f32a8858fa4f1b7a007443006d2a99
deafelimuplus <47976598+hudsonasi@users.noreply.github.com>
2024-11-02 00:47:51 +0300

    Deleted and Installed fresh nextjs app

--- src/app/components/index.tsx
+++
@@ -1,6 +0,0 @@
-export { Arrow } from "./Arrow";
-export { ArrowBox } from "./ArrowBox";
-export { Firebase } from "./Firebase";
-export { Header } from "./Header";
-export { Preloader } from "./Preloader";
-export { Features } from "./Features";
