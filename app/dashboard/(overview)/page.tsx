84b65b01d5b19b99a5896e01e833038ef4ff5fcc
deafelimuplus <47976598+hudsonasi@users.noreply.github.com>
2024-11-18 15:47:13 +0300

    Improving Accessibility
    In the code above, you're also adding the following aria labels:
    
    aria-describedby="customer-error": This establishes a relationship between the select element and the error message container. It indicates that the container with id="customer-error" describes the select element. Screen readers will read this description when the user interacts with the select box to notify them of errors.
    
    id="customer-error": This id attribute uniquely identifies the HTML element that holds the error message for the select input. This is necessary for aria-describedby to establish the relationship.
    
    aria-live="polite": The screen reader should politely notify the user when the error inside the div is updated. When the content changes (e.g. when a user corrects an error), the screen reader will announce these changes, but only when the user is idle so as not to interrupt them.
    
    notes:
    
    Add a ternary operator that checks for each specific error. For example, after the customer's field.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,19 +2,20 @@ import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "../../ui/dashboard/revenue-chart";
 import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import { lusitana } from "../../ui/fonts";
-import {fetchCardData } from "../../lib/data";
+// import {fetchCardData } from "../../lib/data";
 import { Suspense } from "react";
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
     // const revenue = await fetchRevenue();
     // const latestInvoices = await fetchLatestInvoices(); // wait for fetchRevenue() to finish
-    const {
+    /* const {
         totalPaidInvoices,
         totalPendingInvoices,
         numberOfCustomers,
         numberOfInvoices
     } = await fetchCardData(); // wait for fetchLatestInvoices() to finish
+     */
 
     return (
         <main>
